183ec4d15270f54b43321fdc1ed50328b92bc512
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-10-05 01:10:52 +0530

    Fix

---
+++ src/RyujinxAndroid/libryujinx/libs/FFmpeg.cmake
@@ -0,0 +1,41 @@
+include(ExternalProject)
+
+# Set the FFmpeg version and repository
+set(FFMPEG_VERSION "7.1")
+set(FFMPEG_GIT_REPOSITORY "https://git.ffmpeg.org/ffmpeg.git")
+
+# Environment setup for Android NDK
+set(PROJECT_ENV "ANDROID_NDK_ROOT=${CMAKE_ANDROID_NDK}")
+
+if (CMAKE_HOST_WIN32)
+    # Handle Windows specific setup
+    ...
+elseif (CMAKE_HOST_UNIX)
+    find_program(MAKE_COMMAND NAMES make REQUIRED)
+    list(APPEND PROJECT_ENV "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}")
+else ()
+    message(WARNING "Host system (${CMAKE_HOST_SYSTEM_NAME}) not supported. Treating as unix.")
+    find_program(MAKE_COMMAND NAMES make REQUIRED)
+    list(APPEND PROJECT_ENV "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}")
+endif ()
+
+ExternalProject_Add(
+    ffmpeg
+    GIT_REPOSITORY              ${FFMPEG_GIT_REPOSITORY}
+    GIT_TAG                     n${FFMPEG_VERSION}
+    LIST_SEPARATOR              "|"
+    CONFIGURE_COMMAND           ${CMAKE_COMMAND} -E env ${PROJECT_ENV}
+                                    ./configure
+                                    --target-os=android
+                                    --arch=${CMAKE_ANDROID_ARCH}
+                                    --enable-shared
+                                    --disable-static
+                                    --prefix=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+                                    --disable-doc
+                                    --disable-programs
+                                    --enable-cross-compile
+                                    --cross-prefix=${ANDROID_TOOLCHAIN_ROOT}/bin/${CMAKE_ANDROID_TOOLCHAIN_PREFIX}- 
+                                    --sysroot=${CMAKE_ANDROID_SYSROOT}
+    BUILD_COMMAND               ${CMAKE_COMMAND} -E env ${PROJECT_ENV} make
+    INSTALL_COMMAND             ${CMAKE_COMMAND} -E env ${PROJECT_ENV} make install
+)
