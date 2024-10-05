e0bdca16681af07a937a8e42e36eea2e529f4a1d
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-10-05 10:33:59 +0530

    Fix

--- src/RyujinxAndroid/libryujinx/libs/OpenAL.cmake
+++ src/RyujinxAndroid/libryujinx/libs/OpenAL.cmake
@@ -5,6 +5,7 @@ set(OPENAL_VERSION "1.23.1")
 set(OPENAL_GIT_REPOSITORY "https://github.com/kcat/openal-soft.git")
 
 # Environment setup for Android NDK
+set(ANDROID_TOOLCHAIN_PREFIX "${CMAKE_ANDROID_TOOLCHAIN_PREFIX}")
 set(PROJECT_ENV "ANDROID_NDK_ROOT=${CMAKE_ANDROID_NDK}")
 
 if (CMAKE_HOST_WIN32)
@@ -14,7 +15,7 @@ elseif (CMAKE_HOST_UNIX)
     find_program(MAKE_COMMAND NAMES make REQUIRED)
     list(APPEND PROJECT_ENV "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}")
 else ()
-    message(WARNING "Host system (${CMAKE_HOST_SYSTEM_NAME}) not supported. Treating as unix.")
+    message(WARNING "Host system (${CMAKE_HOST_SYSTEM_NAME}) not supported. Treating as Unix.")
     find_program(MAKE_COMMAND NAMES make REQUIRED)
     list(APPEND PROJECT_ENV "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}")
 endif ()
@@ -31,6 +32,8 @@ ExternalProject_Add(
                                     -DCMAKE_ANDROID_ARCH_ABI=${CMAKE_ANDROID_ARCH}
                                     -DCMAKE_ANDROID_NDK=${CMAKE_ANDROID_NDK}
                                     -DCMAKE_INSTALL_PREFIX=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+                                    -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -D__ANDROID_API__=${CMAKE_SYSTEM_VERSION}"
+                                    -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -D__ANDROID_API__=${CMAKE_SYSTEM_VERSION}"
     BUILD_COMMAND               ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR}/openal-build --target install
     INSTALL_COMMAND             ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR}/openal-build --target install
 )
