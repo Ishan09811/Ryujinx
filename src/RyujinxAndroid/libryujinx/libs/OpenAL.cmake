6f0028c7dc0ff7ebd2671b85b62b82b86949d4fe
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-10-05 10:47:39 +0530

    Fix

--- src/RyujinxAndroid/libryujinx/libs/OpenAL.cmake
+++ src/RyujinxAndroid/libryujinx/libs/OpenAL.cmake
@@ -5,18 +5,17 @@ set(OPENAL_VERSION "1.23.1")
 set(OPENAL_GIT_REPOSITORY "https://github.com/kcat/openal-soft.git")
 
 # Environment setup for Android NDK
-set(ANDROID_TOOLCHAIN_PREFIX "${CMAKE_ANDROID_TOOLCHAIN_PREFIX}")
 set(PROJECT_ENV "ANDROID_NDK_ROOT=${CMAKE_ANDROID_NDK}")
 
 if (CMAKE_HOST_WIN32)
     # Handle Windows specific setup
     # Your Windows-specific code here...
 elseif (CMAKE_HOST_UNIX)
-    find_program(MAKE_COMMAND NAMES make REQUIRED)
+    find_program(NINJA_COMMAND NAMES ninja REQUIRED)
     list(APPEND PROJECT_ENV "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}")
 else ()
-    message(WARNING "Host system (${CMAKE_HOST_SYSTEM_NAME}) not supported. Treating as Unix.")
-    find_program(MAKE_COMMAND NAMES make REQUIRED)
+    message(WARNING "Host system (${CMAKE_HOST_SYSTEM_NAME}) not supported. Treating as unix.")
+    find_program(NINJA_COMMAND NAMES ninja REQUIRED)
     list(APPEND PROJECT_ENV "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}")
 endif ()
 
@@ -29,11 +28,10 @@ ExternalProject_Add(
     CONFIGURE_COMMAND           ${CMAKE_COMMAND} -E env ${PROJECT_ENV}
                                     cmake -B${CMAKE_BINARY_DIR}/openal-build -H${CMAKE_BINARY_DIR}/openal-src
                                     -DCMAKE_SYSTEM_NAME=Android
-                                    -DCMAKE_ANDROID_ARCH_ABI=${CMAKE_ANDROID_ARCH}
+                                    -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a  # Changed here
                                     -DCMAKE_ANDROID_NDK=${CMAKE_ANDROID_NDK}
                                     -DCMAKE_INSTALL_PREFIX=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
-                                    -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -D__ANDROID_API__=${CMAKE_SYSTEM_VERSION}"
-                                    -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -D__ANDROID_API__=${CMAKE_SYSTEM_VERSION}"
+                                    -G Ninja  # Added here to use Ninja
     BUILD_COMMAND               ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR}/openal-build --target install
     INSTALL_COMMAND             ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR}/openal-build --target install
 )
